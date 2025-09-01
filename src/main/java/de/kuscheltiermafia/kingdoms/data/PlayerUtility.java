ebecf4134ec59cf74f999a1ba484307acb35b678
Morgon <moritz.olthoff@gmail.com>
2025-09-01 14:22:18 +0200

    Fix player Stats, remove error messages

--- src/main/java/de/kuscheltiermafia/kingdoms/data/PlayerUtility.java
+++ src/main/java/de/kuscheltiermafia/kingdoms/data/PlayerUtility.java
@@ -3,6 +3,7 @@ package de.kuscheltiermafia.kingdoms.data;
 import de.kuscheltiermafia.kingdoms.Kingdoms;
 import org.bukkit.entity.Player;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,7 +27,26 @@ public class PlayerUtility {
     }
 
     public static String getFolderPath(Player player) {
-        return Kingdoms.getPlugin().getDataFolder().getAbsolutePath() + "/player/" + player.getUniqueId() + "/";
+        String path = Kingdoms.getPlugin().getDataFolder().getAbsolutePath() + "/player/" + player.getUniqueId() + "/";
+        File dir = new File(path);
+        if (!dir.exists()) dir.mkdirs();
+        return path;
+    }
+
+
+    public static boolean initPlayer(Player player) {
+        try {
+            boolean success = PlayerStats.setupConfig(player);
+            if (!success) {
+                player.kickPlayer("Could not load your player data. Please try again. If this problem persists, please contact an administrator.");
+                Kingdoms.getPlugin().getLogger().warning("Could not load player data for " + player.getName());
+                return false;
+            }
+        }catch (Exception e) {
+            Kingdoms.getPlugin().getLogger().warning("Could not load player data for " + player.getName());
+            return false;
+        }
+        return true;
     }
 
 }
